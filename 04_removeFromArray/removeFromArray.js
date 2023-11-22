7b2ae02ee42a00c63f2ef6be53acd1819e7fa897
MedHam94 <hammas.m.mh@gmail.com>
2023-11-22 16:20:44 +0100

    solve 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,5 @@
 const removeFromArray = function(arr, ...num) {
     return arr.filter(el => !num.includes(el))
-
-
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let newArr = []
 
+    for(let i = Math.min(num1, num2); i<= Math.max(num1,num2); i++){
+        newArr.push(i)
+    }
+console.log(newArr);
+    if((num1 & num2 >= 0) || (typeof num1 & typeof num2 === Number)){
+        
+        return newArr.reduce((acc ,cur)=> acc + cur)
+
+    }else {
+
+        return 'ERROR'
+
+    }
+   
 };
 
 // Do not edit below this line
